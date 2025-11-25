f91ea14e4b93bcec392164e37bc3b93cfdfe563d
herve <108332605+cx-herve-jouanin@users.noreply.github.com>
2025-11-25 18:27:40 +0100

    Add comment placeholder in ResearchHandler for clarity

--- app/routes/research.js
+++ app/routes/research.js
@@ -13,6 +13,7 @@ function ResearchHandler(db) {
 
         if (req.query.symbol) {
             const url = req.query.url + req.query.symbol;
+            //
             return needle.get(url, (error, newResponse, body) => {
                 if (!error && newResponse.statusCode === 200) {
                     res.writeHead(200, {
